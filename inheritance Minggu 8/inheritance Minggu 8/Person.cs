174f938ea50d2d1a6e2438497720fe55ee8a52cc
rizkynandaanggia <127828416+rizkynandaanggia@users.noreply.github.com>
2023-06-05 22:49:40 +0700

    Add files via upload

---
+++ inheritance Minggu 8/inheritance Minggu 8/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_Tugas_minggu_8
+{
+    public class Person
+    {
+        // property
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // fungsi
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama Saya {0}, dan umur saya {1}, tahun", Name, Age);
+        }
+    }
+    // class turunan
+    public class karyawan : Person
+    {
+        // property class karyawan : Person
+        public string karyawanID { get; set; }
+        public string subject { get; set; }
+
+        //overfride method
+        public void InfoPerson(string subject)
+        {
+            //this
+            this.subject = subject;
+            Console.WriteLine("Nama Saya {0}, dan umur saya {1}, tahun", this.subject);
+        }
+    }
+}
---
+++ inheritance Minggu 8/inheritance Minggu 8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using inheritance_Tugas_minggu_8;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Abdul Mizwar", 24);
+            person1.InfoPerson();
+
+            // Inheritance
+            Karyawan karyawan1 = new Karyawan("Putri", 24, "Dosen");
+            karyawan1.InfoPerson();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void InfoPerson()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+    }
+
+    class Karyawan : Person
+    {
+        public string JobTitle { get; set; }
+
+        public Karyawan(string name, int age, string jobTitle) : base(name, age)
+        {
+            JobTitle = jobTitle;
+        }
+
+        public void InfoPerson()
+        {
+            base.InfoPerson();
+            Console.WriteLine("Job Title: " + JobTitle);
+        }
+    }
+}
